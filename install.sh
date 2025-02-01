bbb9ca6d5d022d10d946d1bb3a65d494ca3229c9
luwai <luwaiwong@gmail.com>
2025-01-31 20:32:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
-
-# Install yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-
-# Install hyprland essentials
-yay -S \
-	hyprland
-	
-#
+# Copy config files to ~/.config
+cp -r ./BetterDiscord ~/.config
+cp -r ./cava ~/.config
+cp -r ./hypr ~/.config
+cp -r ./kitty ~/.config
+cp -r ./omposh ~/.config
+cp -r ./rofi ~/.config
+cp -r ./spicetify ~/.config
+cp -r ./waybar ~/.config
+cp -r ./wlogout ~/.config

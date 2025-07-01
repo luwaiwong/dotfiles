7f346cd6c83d20e9b3b7e794085aacafc2248c45
luwai <luwaiwong@gmail.com>
2025-07-01 11:49:22 -0400

    burh

---
+++ quickshell/launchshell.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+QT_SCALE_FACTOR=1 quickshell
---
+++ save.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# list of apps to update
+list="cava hypr kitty omposh rofi spicetify quickshell wlogout"
+
+
+# Delete existing config files in dotfiles directory
+for dir in $list; do
+    rm -rf ./$dir
+done
+
+# Copy config files to ~/.config
+
+for dir in $list; do
+    cp -r ~/.config/$dir ./
+done
+
